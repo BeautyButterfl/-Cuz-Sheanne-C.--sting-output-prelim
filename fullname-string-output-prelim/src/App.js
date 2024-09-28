7cdbae8d84e3753758132710d961f1d39ca2bdf3
Your Name <you@example.com>
2024-09-28 10:30:20 +0800

    StringValue

---
+++ fullname-string-output-prelim/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>Hello, this is a string output!</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
